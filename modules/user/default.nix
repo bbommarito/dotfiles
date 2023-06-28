46aee6417d39d84e8dc523e0379e0cb3bb17a399
Brian "Burrito" Bommarito <brian@brianbommarito.xyz>
2023-06-28 13:24:36 -0400

    Forgot to persist a few user directories.

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,40 +1,39 @@
-{
-  config,
-  lib,
-  myData,
-  pkgs,
-  ...
+{ config
+, lib
+, myData
+, pkgs
+, ...
 }: {
   options.bbommarito.user = {
     enable = lib.mkEnableOption "Enables my user";
     uid =
       lib.mkOption
-      {
-        type = lib.types.nullOr lib.types.int;
-        default = 1000;
-        description = "My user id for this system.";
-      };
+        {
+          type = lib.types.nullOr lib.types.int;
+          default = 1000;
+          description = "My user id for this system.";
+        };
     username =
       lib.mkOption
-      {
-        type = lib.types.str;
-        default = "bbommarito";
-        description = "My username for this system.";
-      };
+        {
+          type = lib.types.str;
+          default = "bbommarito";
+          description = "My username for this system.";
+        };
     realname =
       lib.mkOption
-      {
-        type = lib.types.str;
-        default = "Brian \"Burrito\" Bommarito";
-        description = "My realname for this system.";
-      };
+        {
+          type = lib.types.str;
+          default = "Brian \"Burrito\" Bommarito";
+          description = "My realname for this system.";
+        };
     email =
       lib.mkOption
-      {
-        type = lib.types.str;
-        default = "brian@brianbommarito.xyz";
-        description = "My email for this system.";
-      };
+        {
+          type = lib.types.str;
+          default = "brian@brianbommarito.xyz";
+          description = "My email for this system.";
+        };
     signingKey = lib.mkOption {
       type = lib.types.str;
       default = "675F2E6CBA45FC655AAA65C508344FF6D0E3107F";
@@ -42,16 +41,16 @@
     };
     extraGroups = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [];
+      default = [ ];
     };
     extraAuthorizedKeys = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [];
+      default = [ ];
       description = "Additional authorized keys.";
     };
     extraUserPackages = lib.mkOption {
       type = lib.types.listOf lib.types.package;
-      default = [];
+      default = [ ];
       description = "Extra packages to install in my users profile.";
     };
   };
@@ -64,12 +63,18 @@
 
     users.users.${config.bbommarito.user.username} =
       lib.mkIf config.bbommarito.user.enable
-      {
-        description = "${config.bbommarito.user.realname},,,,";
-        extraGroups = ["wheel"];
-        isNormalUser = true;
-        passwordFile = config.age.secrets.hashed-bbommarito-password.path;
-        uid = config.bbommarito.user.uid;
-      };
+        {
+          description = "${config.bbommarito.user.realname},,,,";
+          extraGroups = [ "wheel" ];
+          isNormalUser = true;
+          passwordFile = config.age.secrets.hashed-bbommarito-password.path;
+          uid = config.bbommarito.user.uid;
+        };
+
+    bbommarito.base.zfs.user.directories = [
+      ".dotfiles"
+      ".ssh"
+      ".local/share/fonts"
+    ];
   };
 }
